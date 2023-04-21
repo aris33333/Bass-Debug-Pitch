b3c47806bc415bef3e1c8bea1c49e38143cc134e
Saketh Aripirala <saketharipirala101@gmail.com>
2023-04-21 16:47:48 +0300

    Writing

--- Scripts/yin.py
+++ Scripts/yin.py
@@ -1,6 +1,7 @@
 from scipy import signal
 from scipy.io import wavfile
 import numpy as np
+import matplotlib.pyplot
 
 def f(x):
     f_0 = 1
@@ -31,22 +32,9 @@ def detect_pitch(f, W, t, fs, bounds, thresh = 0.1):
             sample = np.argmin(CMNDF_vals) + bounds[0]
     return fs / sample
 
-fs, data = wavfile.read('sounds/segments/MAIN_OUT_SEGMENT.wav')
-data = data.astype(np.float64)
-W = int(5 / 2048 * fs)
+W = 32
+
+data = f(10)
 bounds = [10, 500]
 
-pitches = []
-
-for i in range(data.shape[0] // (W + 3)):
-    pitches.append(
-        detect_pitch(
-        data, 
-        W, 
-        i * W,
-        fs,
-        bounds
-        )
-    )
-
-print(pitches)
+detect_pitch(data, W)
