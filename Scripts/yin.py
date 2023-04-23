990bc14cdc00d602803b2e5c226241348a0ddee2
Saketh Aripirala <saketharipirala101@gmail.com>
2023-04-23 21:05:54 +0300

    Writing

--- Scripts/yin.py
+++ Scripts/yin.py
@@ -1,7 +1,7 @@
 from scipy import signal
 from scipy.io import wavfile
 import numpy as np
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
 def f(x):
     f_0 = 1
@@ -9,10 +9,8 @@ def f(x):
     return np.sin(x * np.pi * 2 * f_0) * envelope(x)
 
 def ACF(f, W, t, lag):
-    return np.sum(
-        f[t : t + W] * f[lag + t : lag + t + W]
-    )
-
+    return np.sum(f[t : t + W] * f[lag + t : lag + t + W])
+  
 def DF(f, W, t, lag):
     return ACF(f, W, t, 0) + ACF(f, W, t + lag, 0) - (2 * ACF(f, W, t, lag))
 
@@ -21,7 +19,7 @@ def CMNDF(f, W, t, lag):
         return 1
     return DF(f, W, t, lag) / np.sum([DF(f, W, t, j+1) for j in range(lag)]) * lag
 
-def detect_pitch(f, W, t, fs, bounds, thresh = 0.1):
+def detect_pitch(f, W, t, fs, bounds, thresh = 1):
     CMNDF_vals = [CMNDF(f, W, t, i) for i in range(*bounds)]
     sample = None
     for i, val in enumerate(CMNDF_vals):
@@ -32,9 +30,15 @@ def detect_pitch(f, W, t, fs, bounds, thresh = 0.1):
             sample = np.argmin(CMNDF_vals) + bounds[0]
     return fs / sample
 
-W = 32
+fs = 500
+start = 0
+end = 5
+samples = int(fs * (end - start) + 1)
+W = 200
+bounds = [20, samples // 2]
 
-data = f(10)
-bounds = [10, 500]
+x = np.linspace(start, end, samples)
+print(detect_pitch(f(x), W, 1, fs, bounds))
 
-detect_pitch(data, W)
+plt.plot(f(x))
+plt.show()
